8075da0e79bb0e395fde4d91a06851fa25545f28
Steven Webb <swebb@YDKM.local>
2017-06-30 17:44:22 +0100

    this is a test

---
+++ ctest/myfuncs1.c
@@ -0,0 +1,9 @@
+//
+//  myfuncs1.c
+//  ctest
+//
+//  Created by Steven Webb on 05/04/2017.
+//  Copyright © 2017 Steven Webb. All rights reserved.
+//
+
+#include "myfuncs1.h"
---
+++ ctest/myfuncs1.h
@@ -0,0 +1,14 @@
+//
+//  myfuncs1.h
+//  ctest
+//
+//  Created by Steven Webb on 05/04/2017.
+//  Copyright © 2017 Steven Webb. All rights reserved.
+//
+
+#ifndef myfuncs1_h
+#define myfuncs1_h
+
+#include <stdio.h>
+
+#endif /* myfuncs1_h */
